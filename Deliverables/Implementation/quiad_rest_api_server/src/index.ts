e8c55e767bb40be5aed1ada54aac57449de7e555
daqh <valeriodipasq@gmail.com>
2022-12-02 19:18:19 +0100

    Prima configurazione del server

--- Deliverables/Implementation/quiad_rest_api_server/src/index.ts
+++ Deliverables/Implementation/quiad_rest_api_server/src/index.ts
@@ -3,17 +3,8 @@ import express, { Express } from "express";
 
 const prisma = new PrismaClient();
 
-(async () => {
-    const nodo = await prisma.node.findFirst({
-        where: {
-            FatherHasChildren: undefined,
-            AND: {
-                MotherHasChildren: undefined,
-            }
-        },
-        include: {
-            father: true,
-        }
-    });
-    console.log(nodo);
-})();
+const app = express();
+
+app.listen(8080, () => {
+    console.info("Server is running on port ${8080}");
+});
