a838992688f8beb188120a11760612ad61aba468
daqh <valeriodipasq@gmail.com>
2022-12-26 18:03:09 +0100

    Impostazione unit testing per REST API

---
+++ Deliverables/Implementation/quiad_rest_api_server/test/index.spec.ts
@@ -0,0 +1,8 @@
+// import {  } from "mocha";
+import { AuthService } from "../src/account/services/auth.service"
+
+describe("First test", () => {
+    it("Should work", () => {
+        
+    })
+});
